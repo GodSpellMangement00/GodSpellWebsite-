b786162f5a33ae540c066bd282d12904d09af6c5
Deadslot <singhvarinder05338@gmail.com>
2026-01-14 08:19:27 +0530

    Add lightbox open and close functions

--- script.js
+++ script.js
@@ -130,3 +130,13 @@ if (galleryImages.length) {
     overlay.style.display = "none";
   });
 }
+function openLightbox(src) {
+  const box = document.getElementById("lightbox");
+  const img = document.getElementById("lightbox-img");
+  img.src = src;
+  box.style.display = "flex";
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
